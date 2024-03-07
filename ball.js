d6a3996810480500c0b5383c2f24b884ebf4b376
Animish <animishprateek@gmial.com>
2024-03-08 01:17:37 +0530

    Implemeted kill logic

--- ball.js
+++ ball.js
@@ -1,8 +1,9 @@
 class Ball {
-    constructor(element, y) {
+    constructor(element, x, y) {
         this.element = element;
+        this.text = element.textContent;
         this.element.style.setProperty('--top', y);
-
+        this.element.style.setProperty('--left', x);
     }
     set position_x(x) {
         this.element.style.setProperty('--left', x);
@@ -10,18 +11,32 @@ class Ball {
     set position_y(y) {
         this.element.style.setProperty('--top', y);
     }
+
     get position_x() {
         return parseFloat(getComputedStyle(this.element).getPropertyValue('--left'));
+    }
+
+    getText() {
+        return this.text;
     }
 
     moveBallRight(speed, time) {
         const currentX = this.position_x;
         this.position_x = (currentX + speed * time);
 
-        if (currentX > 99.99) {
-            this.position_x = 0;
-            this.position_y = Math.floor(Math.random() * 90);
+        if (currentX > 80) {
+            this.position_x = 10;
+            this.position_y = Math.floor(Math.random() * 50 + 20);
+            this.element.style.setProperty('--display', 'block');
+            this.element.style.setProperty('--ballBg', '#e0e0f050');
         }
+    }
+
+    kill() {
+        this.element.style.setProperty('--ballBg', '#f01515');
+        setTimeout(() => {
+            this.element.style.setProperty('--display', 'none');
+        }, 200);
     }
 }
 
--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 import { Ball } from "./ball.js";
 
-const Ball1 = document.getElementById('ball');
-const ballElement = new Ball(Ball1, 10, Math.floor(Math.random() * 90))
-console.log(ballElement);
+const Ball1 = document.getElementById('ball1');
+const Ball2 = document.getElementById('ball2');
+const Ball3 = document.getElementById('ball3');
+const ballElement1 = new Ball(Ball1, 10, Math.floor(Math.random() * 50 + 20))
+const ballElement2 = new Ball(Ball2, 33, Math.floor(Math.random() * 50 + 20))
+const ballElement3 = new Ball(Ball3, 56, Math.floor(Math.random() * 50 + 20))
 
-let speed = 0.04;
+const kill = document.getElementById('kill');
+let scoreElement = document.getElementById('score');
+let score = 0;
+
+let speed = 0.025;
 let lastTime = null;
 
+kill.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-// function updatePosition(element, speed, time) {
-// const currentX = this.element.style.getProperty('--left');
-
-//     this.position_x(currentX + speed + time);
-// }
+    const { srcElement } = e;
+    if (ballElement1.text === srcElement[0].value) {
+        ballElement1.kill();
+        score += 1;
+    }
+    if (ballElement2.text === srcElement[0].value) {
+        ballElement2.kill();
+        score += 1;
+    }
+    if (ballElement3.text === srcElement[0].value) {
+        ballElement3.kill();
+        score += 1;
+    }
+    srcElement[0].value = '';
+    scoreElement.innerText = score;
+})
 
 function update(time) {
     if (!lastTime) {
@@ -20,8 +40,9 @@ function update(time) {
         window.requestAnimationFrame(update);
         return;
     }
-    // console.log(time - lastTime);
-    ballElement.moveBallRight(speed, time - lastTime);
+    ballElement1.moveBallRight(speed, time - lastTime);
+    ballElement2.moveBallRight(speed, time - lastTime);
+    ballElement3.moveBallRight(speed, time - lastTime);
     lastTime = time;
     window.requestAnimationFrame(update);
 }
